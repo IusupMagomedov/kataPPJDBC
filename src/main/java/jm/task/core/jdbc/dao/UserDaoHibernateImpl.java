f38861fefbafd2990583494696bf11631c5d515b
iusupmagomedov <yusub1988@gmail.com>
2024-11-16 11:14:56 +0300

    feat: util and servise are ready

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,12 +1,16 @@
 package jm.task.core.jdbc.dao;
 
+import com.mysql.cj.Query;
+import com.mysql.cj.Session;
 import jm.task.core.jdbc.model.User;
-
+import org.hibernate.SessionFactory;
+import jm.task.core.jdbc.util.HibernateUtil;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private Query query
     public UserDaoHibernateImpl() {
-
     }
 
 
@@ -22,12 +26,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = sessionFactory.getSessionFactory().openSession()) {    //пытаемся получить сессию, подключиться к базе
+            transaction = session.beginTransaction();
+            и потом коммитим соответственно либо сессию, либо транзакцию
+            В эксепшине проверяем на null, если что-то есть, делаем rollback
     }
 
     @Override
